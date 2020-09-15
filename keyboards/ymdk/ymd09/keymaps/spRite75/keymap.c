44c387aecb8e549175ebf6e2f4704bf858dea352
Luke Montgomery <luke.montgomery@shinesolutions.com>
2020-09-15 11:40:02 +1000

    just toggle layer

--- keyboards/ymdk/ymd09/keymaps/spRite75/keymap.c
+++ keyboards/ymdk/ymd09/keymaps/spRite75/keymap.c
@@ -2,12 +2,12 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT(HYPR(KC_KP_7), HYPR(KC_KP_8),        HYPR(KC_KP_9),
-               HYPR(KC_KP_4), LT(1, HYPR(KC_KP_5)), HYPR(KC_KP_6),
-               HYPR(KC_KP_1), HYPR(KC_KP_2),        HYPR(KC_KP_3)),
+  [0] = LAYOUT(HYPR(KC_KP_7), HYPR(KC_KP_8), HYPR(KC_KP_9),
+               HYPR(KC_KP_4), TG(1),         HYPR(KC_KP_6),
+               HYPR(KC_KP_1), HYPR(KC_KP_2), HYPR(KC_KP_3)),
 
   [1] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
-               RGB_HUI, KC_TRNS, RGB_SAI,
-               RGB_HUD, RGB_VAD, RGB_SAD),
+               RGB_HUI,  TG(1),   RGB_SAI,
+               RGB_HUD,  RGB_VAD, RGB_SAD),
 
 };
