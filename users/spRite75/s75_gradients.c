96315c096c14c0d3e81f54d6dfcb4ec3f72331d8
spRite75 <luke.montgomery@hotmail.com>
2021-05-26 08:38:00 +1000

    add white colour option

--- users/spRite75/s75_gradients.c
+++ users/spRite75/s75_gradients.c
@@ -8,40 +8,47 @@ RGB yellow(void) { RGB col = {                .r = 255,   .g = 255,     .b = 0  
 RGB cyan(void) { RGB col = {                  .r = 0,     .g = 255,     .b = 255    }; return col; };
 RGB blue(void) { RGB col = {                  .r = 0,     .g = 0,       .b = 255    }; return col; };
 RGB magenta(void) { RGB col = {               .r = 255,   .g = 0,       .b = 255    }; return col; };
+RGB white(void) { RGB col = {                 .r = 180,   .g = 200,     .b = 200    }; return col; };
 
-s75_gradient_point pink_lemonade[] = { 
-    { .s_p = 0.00, .e_p = 0.33, .s_rgb = iceBlue, .e_rgb = iceBlue }, 
-    { .s_p = 0.33, .e_p = 0.66, .s_rgb = iceBlue, .e_rgb = lemonadePink }, 
-    { .s_p = 0.66, .e_p = 1.00, .s_rgb = lemonadePink, .e_rgb = lemonadePink }, 
-    { .end = true } 
+s75_gradient_point full_white[] = {
+    { .s_p = 0.00, .e_p = 1.00, .s_rgb = white, .e_rgb = white },
+    { .end = true }
 };
 
-s75_gradient_point sex_on_the_beach[] = { 
+s75_gradient_point pink_lemonade[] = {
+    { .s_p = 0.00, .e_p = 0.33, .s_rgb = iceBlue, .e_rgb = iceBlue },
+    { .s_p = 0.33, .e_p = 0.66, .s_rgb = iceBlue, .e_rgb = lemonadePink },
+    { .s_p = 0.66, .e_p = 1.00, .s_rgb = lemonadePink, .e_rgb = lemonadePink },
+    { .end = true }
+};
+
+s75_gradient_point sex_on_the_beach[] = {
     { .s_p = 0.00, .e_p = 1.00, .s_rgb = sexOnTheBeach_start, .e_rgb = sexOnTheBeach_end },
-    { .end = true } 
+    { .end = true }
 };
 
-s75_gradient_point capsicum[] = { 
+s75_gradient_point capsicum[] = {
     { .s_p = 0.00, .e_p = 0.33, .s_rgb = red, .e_rgb = red },
     { .s_p = 0.33, .e_p = 0.66, .s_rgb = green, .e_rgb = green },
     { .s_p = 0.66, .e_p = 1.00, .s_rgb = yellow, .e_rgb = yellow },
-    { .end = true } 
+    { .end = true }
 };
 
 // This mimics the default rainbow pattern that comes on the Drop Alt
-s75_gradient_point massdrop_rainbow[] = { 
+s75_gradient_point massdrop_rainbow[] = {
     { .s_p = 0.000, .e_p = 0.167, .s_rgb = red, .e_rgb = yellow },
     { .s_p = 0.167, .e_p = 0.333, .s_rgb = yellow, .e_rgb = green },
     { .s_p = 0.333, .e_p = 0.500, .s_rgb = green, .e_rgb = cyan },
     { .s_p = 0.500, .e_p = 0.667, .s_rgb = cyan, .e_rgb = blue },
     { .s_p = 0.667, .e_p = 0.833, .s_rgb = blue, .e_rgb = magenta },
     { .s_p = 0.833, .e_p = 1.000, .s_rgb = magenta, .e_rgb = red },
-    { .end = true } 
+    { .end = true }
 };
 
 void *gradient_setups[] = {
-    pink_lemonade,
     sex_on_the_beach,
+    pink_lemonade,
+    full_white,
     capsicum,
     massdrop_rainbow
 };
